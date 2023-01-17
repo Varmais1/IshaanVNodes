17b052c38da2cba0772c4a6c26d5aa4946cfaabd
Varmais1 <418487@bsd48.org>
2023-01-17 21:01:17 +0000

    Nodes

---
+++ Node.cpp
@@ -0,0 +1,33 @@
+/*Name: Ishaan Varma
+  Date: 1/17/2023
+  Purpose: Where the Node class is implemented
+*/
+//includes
+#include "Node.h"
+#include <iostream>
+
+//constructors
+Node::Node() {
+  student = NULL;
+  next = NULL;
+}
+
+Node::Node(Student* nstudent, Node* nnext) {
+  student = nstudent;
+  next = nnext;
+}
+
+//gets the student in the node
+Student* Node::getStudent() {
+  return student;
+}
+
+//gets the next node
+Node* Node::getNext() {
+  return next;
+}
+
+//sets the next node to the node you want
+void Node::setNext(Node* nnext) {
+  next = nnext;
+}
---
+++ Node.h
@@ -0,0 +1,27 @@
+/*Name: Ishaan Varma
+  Date: 1/17/2023
+  Purpose: Where the Node class is defined
+*/
+//header gaurd and includes
+#ifndef NODE_H
+#define NODE_H
+#include <cstring>
+#include "Student.h"
+
+//creates the node class
+class Node {
+ public:
+  //constructors
+  Node();
+  Node(Student* nstudent, Node* nnext);
+  //getters and setters
+  Student* getStudent();
+  Node* getNext();
+  void setNext(Node* nnext);
+ private:
+  //the content of the node(the student)
+  Student* student;
+  //the next node(NULL if there is no next node)
+  Node* next;
+};
+#endif
